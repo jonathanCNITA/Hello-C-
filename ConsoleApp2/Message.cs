352fc67ca8c9144d8f1b140839f28482397edb5f
Jonathan Courat <jonathan.courat@Digital-Grenoble.local>
2018-08-22 10:16:39 +0200

    Change the way with the Message Class

---
+++ ConsoleApp2/Message.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Message
+    {
+        public string GetHelloMessage()
+        {
+           
+            DateTime Now = DateTime.Now;
+
+            string MessageToReturn = "no";
+            string UserName = Environment.UserName;
+
+            if( Now.DayOfWeek == DayOfWeek.Saturday
+                || Now.DayOfWeek == DayOfWeek.Sunday
+                || Now.DayOfWeek == DayOfWeek.Friday && Now.Hour > 18
+                || Now.DayOfWeek == DayOfWeek.Monday && Now.Hour < 9
+                )
+            {
+                MessageToReturn = "Bon weekend " + UserName;
+            }
+            else if (Now.Hour >= 9 && Now.Hour < 13)
+            {
+                MessageToReturn = "Bonjour " + UserName;
+            }
+            else if (Now.Hour >= 13 && Now.Hour < 18)
+            {
+                MessageToReturn = "Bonne aprem " + UserName;
+            }
+            else
+            {
+                MessageToReturn = "Bonsoir " + UserName;
+            }
+            return MessageToReturn;
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,34 +11,12 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Boolean ExitProgram = false;
-            DateTime Now = DateTime.Now;
+            Message greeting = new Message();
 
-            string MessageToReturn = "no";
-            string UserName = Environment.UserName;
-
-            if( Now.DayOfWeek == DayOfWeek.Saturday
-                || Now.DayOfWeek == DayOfWeek.Sunday
-                || Now.DayOfWeek == DayOfWeek.Friday && Now.Hour > 18
-                || Now.DayOfWeek == DayOfWeek.Monday && Now.Hour < 9
-                )
+            while ( !ExitProgram )
             {
-                MessageToReturn = "Bon weekend " + UserName;
-            } else if (Now.Hour >= 9 && Now.Hour < 13 )
-            {
-                MessageToReturn = "Bonjour " + UserName;
-            }
-            else if (Now.Hour >= 13 && Now.Hour < 18)
-            {
-                MessageToReturn = "Bonne aprem " + UserName;
-            } else
-            {
-                MessageToReturn = "Bonsoir " + UserName;
-            }
-
-            while( !ExitProgram )
-            {
-                Console.WriteLine(Now.Hour);
-                Console.WriteLine(MessageToReturn);
+                
+                Console.WriteLine(greeting.GetHelloMessage());
                 Console.WriteLine("Write 'exit' to quit the program");
                 string userChoice = Console.ReadLine();
                 ExitProgram = (userChoice == "exit");
