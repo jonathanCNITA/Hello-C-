a4e043e76b34d4e6dc006c6442a1aeaf30444f96
Jonathan Courat <jonathan.courat@Digital-Grenoble.local>
2018-08-22 09:39:06 +0200

    first commit with log DateTime Now

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Now = DateTime.Now;
+
+            Console.WriteLine(Now);
+        }
+    }
+}
