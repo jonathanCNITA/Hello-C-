a3b5661ec03673fe89a7be64b6982b71e0b91027
Jonathan Courat <jonathan.courat@Digital-Grenoble.local>
2018-08-22 10:04:07 +0200

    log the part of the day + username

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,7 +12,30 @@ namespace ConsoleApp2
         {
             DateTime Now = DateTime.Now;
 
-            Console.WriteLine(Now);
+            string MessageToReturn = "no";
+            string UserName = Environment.UserName;
+
+            if( Now.DayOfWeek == DayOfWeek.Saturday
+                || Now.DayOfWeek == DayOfWeek.Sunday
+                || Now.DayOfWeek == DayOfWeek.Friday && Now.Hour > 18
+                || Now.DayOfWeek == DayOfWeek.Monday && Now.Hour < 9
+                )
+            {
+                MessageToReturn = "Bon weekend " + UserName;
+            } else if (Now.Hour >= 9 && Now.Hour < 13 )
+            {
+                MessageToReturn = "Bonjour " + UserName;
+            }
+            else if (Now.Hour >= 13 && Now.Hour < 18)
+            {
+                MessageToReturn = "Bonne aprem " + UserName;
+            } else
+            {
+                MessageToReturn = "Bonsoir " + UserName;
+            }
+
+            Console.WriteLine(Now.Hour);
+            Console.WriteLine(MessageToReturn);
         }
     }
 }
